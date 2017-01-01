20122fc45c26e8d58d82345fc81e96f6cd122124
Alexander Polynomdivision <digitalmail555@googlemail.com>
2017-01-01 21:27:40 +0100

    Add some stupid source and test code

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+///<reference path="../typings/index.d.ts" />
+let os = require("os");
+
+// Print some stuff
+console.log("I'm being written in TypeScript");
+console.log(`Running on ${os.platform()}`)
---
+++ test/test_is_thirteen.ts
@@ -0,0 +1,7 @@
+let assert = require("assert");
+
+describe("Test", function () {
+	it("should check if -1 is 13", function () {
+		assert.equal(-1, 13);
+	});
+});
