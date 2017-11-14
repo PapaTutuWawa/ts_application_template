1b20ad6b22519782c9138a0f3c70286949e758a6
Alexander Polynomdivision <digitalmail555@googlemail.com>
2017-11-14 21:04:42 +0100

    Clean up and improve the template
    - Fix an issue with mocha not running the tests
    - Add tslint and just steal the tslint.json from google-style
    - Put lib and js_tests to dist/lib/ and dist/tests/
    - Fix linting issues with the tests
    - Rename bin/ts-template to bin/example

--- tests/test_is_thirteen.ts
+++ tests/test_is_thirteen.ts
@@ -1,7 +1,7 @@
 let assert = require("assert");
 
-describe("Test", function () {
-	it("should check if -1 is 13", function () {
-		assert.equal(-1, 13);
-	});
+describe("Test", () => {
+	  it("should check if -1 is 13", () => {
+		    assert.equal(-1, 13);
+	  });
 });
